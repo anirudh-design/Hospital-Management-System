2cc3eb88948fd010d218b23b63fbfe03e22e020a
anisingh3012 <anisingh30121998@gmail.com>
2022-09-09 06:07:30 +0530

    commit

---
+++ src/hospital_mgmt_system/Hospital_Mgmt_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hospital_mgmt_system;
+
+/**
+ *
+ * @author rambo
+ */
+public class Hospital_Mgmt_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
